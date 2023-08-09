6a7966a3658ee57afd8e15e0283f4bce3880a36e
maulan1208 <nguyenlan12082003@gmail.com>
2023-08-09 22:30:50 +0700

    hello

--- src/main/java/edu/vti/rocket/democl3/DemoCl3Application.java
+++ src/main/java/edu/vti/rocket/democl3/DemoCl3Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoCl3Application {
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(DemoCl3Application.class, args);
     }
 
